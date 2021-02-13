ee1453af238ff4f766ec168ab27d9888c0420aab
sajjadsalehi <salehi.sajjad.ai@gmail.com>
2021-02-13 16:48:16 +0100

    Weighted Quick Union

--- WeightedQuickUnion.py
+++ WeightedQuickUnion.py
@@ -1,11 +1,14 @@
 class QuickUnion:
     def __init__(self, n):
         self.id = []
+        self.size = []
         for i in range(n):
             self.id.append(i)
+            self.size.append(1)
 
     def root(self, i):
         while i != self.id[i]:
+            self.id[i] = self.id[self.id[i]]
             i = self.id[i]
         return i
 
@@ -15,8 +18,14 @@ class QuickUnion:
     def union(self, p, q):
         i = self.root(p)
         j = self.root(q)
-
-        self.id[i] = j
+        if i == j:
+            return
+        if self.size[i] < self.size[j]:
+            self.id[i] = j
+            self.size[j] += self.size[i]
+        else:
+            self.id[j] = i
+            self.size[i] += self.size[j]
 
 
 if __name__ == '__main__':
@@ -25,4 +34,3 @@ if __name__ == '__main__':
     quickUnion.union(0, 2)
     quickUnion.union(0, 5)
     print(quickUnion.connected(2, 6))
-
