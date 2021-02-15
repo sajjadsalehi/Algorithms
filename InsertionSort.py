d51a61aab8215461606459ae092ad2ad6c01767a
sajjadsalehi <salehi.sajjad.ai@gmail.com>
2021-02-15 23:49:59 +0100

    Create InsertionSort.py

---
+++ InsertionSort.py
@@ -0,0 +1,20 @@
+class InsertionSort:
+    def sort(self, a):
+        n = len(a)
+        for i in range(n):
+            for j in range(i, n):
+                if a[j] < a[j-1]:
+                    self.swap(a, j, j-1)
+        return a
+
+    def swap(self, a, i, j):
+        temp = a[j]
+        a[j] = a[i]
+        a[i] = temp
+
+
+if __name__ == '__main__':
+    insertion_sort = InsertionSort()
+    a = [1, 90, 73, 12, 2, 24]
+    sorted_array = insertion_sort.sort(a)
+    print(sorted_array)
