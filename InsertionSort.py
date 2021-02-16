088df912e8f82e52802d8c0e65746ec03b7ae18b
sajjadsalehi <salehi.sajjad.ai@gmail.com>
2021-02-16 16:05:22 +0100

    Update InsertionSort.py

--- InsertionSort.py
+++ InsertionSort.py
@@ -2,7 +2,7 @@ class InsertionSort:
     def sort(self, a):
         n = len(a)
         for i in range(n):
-            for j in range(i, n):
+            for j in range(i, 0, -1):
                 if a[j] < a[j-1]:
                     self.swap(a, j, j-1)
                 else:
