b74f199b30404c7163d912fc7f856e425bf4ab9c
sajjadsalehi <salehi.sajjad.ai@gmail.com>
2021-02-17 22:21:41 +0100

    Create MergeSort.py

---
+++ MergeSort.py
@@ -0,0 +1,42 @@
+class MergeSort:
+
+    def sort(self, a):
+        aux = [None]*len(a)
+        self.msort(a, aux, 0, len(a))
+        return a
+
+    def msort(self, a, aux, low, high):
+        if high <= low:
+            return
+        mid = int(low + (high - low)/2)
+        self.msort(a, aux, mid+1, high)
+        self.msort(a, aux, low, mid)
+        self.merge(a, aux, low, mid, high)
+
+    def merge(self, a, aux, low, mid, high):
+        for k in range(low, high):
+            aux[k] = a[k]
+
+        i = low
+        j = mid+1
+        for k in range(low, high):
+            if i > mid:
+                a[k] = aux[j]
+                j += 1
+            elif j >= high:
+                a[k] = aux[i]
+                i += 1
+            elif (aux[j] is not None
+                  and aux[i] is not None) and aux[j] < aux[i]:
+                a[k] = aux[j]
+                j += 1
+            else:
+                a[k] = a[i]
+                i += 1
+
+
+if __name__ == '__main__':
+    merge_sort = MergeSort()
+    a = [1, 90, 73, 12, 2, 24]
+    sorted_array = merge_sort.sort(a)
+    print(sorted_array)
