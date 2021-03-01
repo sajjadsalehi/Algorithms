72f0e4729dfb404812b593b02c0b38f5a4571b01
sajjadsalehi <salehi.sajjad.ai@gmail.com>
2021-03-01 23:43:17 +0100

    Create BreadthFirstSearch.py

---
+++ BreadthFirstSearch.py
@@ -0,0 +1,20 @@
+from Graph import *
+
+
+class BreadthFirstSearch:
+
+    def __init__(self, graph, start):
+        self.marked = []
+        self.edgeTo = []
+        self.bfs(graph, start)
+
+    def bfs(self, graph, start):
+        queue = [start]
+        self.marked[start] = True
+        while len(queue) > 0:
+            v = queue.pop(0)
+            for w in graph.adj(v):
+                if not self.marked[w]:
+                    queue.append(w)
+                    self.marked[w] = True
+                    self.edgeTo[w] = v
