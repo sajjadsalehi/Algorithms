853886985e93da270a2e9a2d6d353641930dd99c
sajjadsalehi <salehi.sajjad.ai@gmail.com>
2021-03-14 10:42:47 +0100

    Update test.py

--- test.py
+++ test.py
@@ -1,23 +1,9 @@
-from BST import *
+from Trie import *
 
-bst = Bst()
+trie = Trie()
 
-bst.put("h", "h")
-bst.put("c", "c")
-bst.put("a", "a")
-bst.put("r", "r")
-bst.put("x", "x")
-bst.put("e", "e")
-bst.put("z", "z")
-bst.put("s", "s")
-
-print(bst.min())
-print(bst.max())
-print(bst.floor("j"))
-print(bst.size())
-print(bst.rank("z"))
-print(bst.keys())
-bst.deleteMin()
-bst.delete("s")
-print(bst.keys())
+trie.put("sea", 12)
+trie.put("immune", 21)
+trie.put("saj", 30)
+print(trie.get("sea"))
 
